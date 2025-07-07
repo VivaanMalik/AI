4f8aaa88c0ed3038e37b6656a514ecf6235c3d25
VivaanDaGr8 <malikvivaan@gmail.com>
2025-07-08 03:41:25 +0530

    adjust build folders

--- version 3/build.bat
+++ version 3/build.bat
@@ -1,4 +1,5 @@
 @REM x64 Native Tools Command Prompt for VS 2022 
 
 @echo off
-nvcc -std=c++17 -I./sqlite3 sqlite3/sqlite3.c -o executable .\main.cpp .\SQLiteManager.cpp .\thread.cpp .\Parser.cpp .\Layer.cpp .\activations.cu .\Initializer.cu .\utils.cpp
+nvcc -std=c++17 -I./sqlite3 sqlite3/sqlite3.c -o .\build\executable .\main.cpp .\SQLiteManager.cpp .\thread.cpp .\Parser.cpp .\Layer.cpp .\activations.cu .\Initializer.cu .\utils.cpp
+build\executable.exe
