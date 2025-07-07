883c5b4999bf41f7a00f3d13dc8a313dad2272e6
VivaanDaGr8 <malikvivaan@gmail.com>
2025-07-07 15:50:55 +0530

    basic skeleton

--- version 3/header.hpp
+++ version 3/header.hpp
@@ -11,10 +11,69 @@
 using json = nlohmann::json;
 using namespace std;
 
+// Initializer
+// ===============================================================================
+class InitializerBase {
+    public:
+    virtual json initialize(vector<int> shape) const = 0;
+    virtual ~InitializerBase() {}
+};
+
+// TODO: add the funcs
+
+// ===============================================================================
+
+// Loss func
+// ===============================================================================
+class LossFuncBase {
+    public:
+    virtual float forward(vector<float> output, vector<float> target_output) const = 0;
+    virtual vector<float> backward() const = 0;
+    virtual ~LossFuncBase() {}
+};
+// ===============================================================================
+
+// lr decay func
+// ===============================================================================
+class LearningRateDecayFuncBase {
+    public:
+    LearningRateDecayFuncBase(float initial_lr);
+    virtual float decay(int timestep) const = 0;
+    virtual ~LearningRateDecayFuncBase() {}
+};
+// ===============================================================================
+
+// weight decay func (add loss)
+// ===============================================================================
+class WeightDecayFuncBase {
+    public:
+    WeightDecayFuncBase(float lambda);
+    virtual float GetAdditionalLoss(vector<vector<float>> weights) const = 0;
+    virtual ~WeightDecayFuncBase() {}
+};
+// ===============================================================================
+
+// Layer
+// ===============================================================================
+class Layer {
+    public: 
+
+};
+// ===============================================================================
+
+
 class Network {
-public:
+    public:
     int id;
+    vector<Layer> Layers;
+    InitializerBase* Initializer;
+    LossFuncBase* LossFunction;
+    LearningRateDecayFuncBase* LearningRateDecayFunction;
+    int EpochNumber;
+    WeightDecayFuncBase* WeightDecayFunction;    
     Network(int id);
+
+    void add_Layer(Layer layer);
     void log_work(sqlite3* db_pointer, float created_on);
     void the_thing_to_be_done(std::string msg);
 };
--- version 3/thread.cpp
+++ version 3/thread.cpp
@@ -1,6 +1,17 @@
 #include "header.hpp"
 
-Network::Network(int id) : id(id) {}
+Network::Network(int id) : 
+    id(id),
+    Layers({}),
+    Initializer(nullptr),
+    LossFunction(nullptr),
+    LearningRateDecayFunction(nullptr),
+    EpochNumber(0),
+    WeightDecayFunction(nullptr) {}
+
+void Network::add_Layer(Layer layer) {
+    this->Layers.push_back(layer);
+}
 
 void Network::log_work(sqlite3* db_pointer, float created_on) {
 
@@ -8,8 +19,6 @@ void Network::log_work(sqlite3* db_pointer, float created_on) {
     write_to_sqlite3_db(db_pointer, values);
     json result = keepchecking(id, db_pointer);
 
-    
-    
     this->the_thing_to_be_done(result["name"]);
 }
 
