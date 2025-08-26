509d823db876705e247d1cf1d4e78aab7d3b7ebe
Vivaan Malik <malikvivaan@gmail.com>
2025-08-26 19:56:24 +0530

    change paths

--- version 3/run.sh
+++ version 3/run.sh
@@ -1,8 +1,1 @@
-while getopts "p:" opt; do
-      case $opt in
-        p) PROCESSES="$OPTARG" ;;
-        \?) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
-      esac
-    done
-
 ./build/executable $PROCESSES
